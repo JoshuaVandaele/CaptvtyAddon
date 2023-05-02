9573cc29cdb461ca9435550f926980df7446c5d0
Joshua Vandaële <joshua@vandaele.software>
2023-05-02 15:26:55 +0200

    chore: Prepare developpement environement

--- buildVars.py
+++ buildVars.py
@@ -10,43 +10,42 @@
 # To avoid initializing translations in this module we simply roll our own "fake" `_` function
 # which returns whatever is given to it as an argument.
 def _(arg):
-	return arg
+    return arg
 
 
 # Add-on information variables
 addon_info = {
-	# add-on Name/identifier, internal for NVDA
-	"addon_name": "addonTemplate",
-	# Add-on summary, usually the user visible name of the addon.
-	# Translators: Summary for this add-on
-	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary": _("Add-on user visible name"),
-	# Add-on description
-	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description": _("""Description for the add-on.
-It can span multiple lines."""),
-	# version
-	"addon_version": "x.y",
-	# Author(s)
-	"addon_author": "name <name@domain.com>",
-	# URL for the add-on documentation support
-	"addon_url": None,
-	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": None,
-	# Documentation file name
-	"addon_docFileName": "readme.html",
-	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": None,
-	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": None,
-	# Add-on update channel (default is None, denoting stable releases,
-	# and for development releases, use "dev".)
-	# Do not change unless you know what you are doing!
-	"addon_updateChannel": None,
-	# Add-on license such as GPL 2
-	"addon_license": None,
-	# URL for the license document the ad-on is licensed under
-	"addon_licenseURL": None,
+    # add-on Name/identifier, internal for NVDA
+    "addon_name": "CaptvtyAddon",
+    # Add-on summary, usually the user visible name of the addon.
+    # Translators: Summary for this add-on
+    # to be shown on installation and add-on information found in Add-ons Manager.
+    "addon_summary": _("Addon permettant de rendre Captvty plus accessible"),
+    # Add-on description
+    # Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+    "addon_description": _("""Addon permettant de rendre Captvty plus accessible"""),
+    # version
+    "addon_version": "0.0",
+    # Author(s)
+    "addon_author": "Joshua Vandaele <folfyblu@gmail.com>",
+    # URL for the add-on documentation support
+    "addon_url": None,
+    # URL for the add-on repository where the source code can be found
+    "addon_sourceURL": "https://github.com/FolfyBlue/CaptvtyAddon",
+    # Documentation file name
+    "addon_docFileName": "readme.html",
+    # Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
+    "addon_minimumNVDAVersion": None,
+    # Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+    "addon_lastTestedNVDAVersion": "2023.1",
+    # Add-on update channel (default is None, denoting stable releases,
+    # and for development releases, use "dev".)
+    # Do not change unless you know what you are doing!
+    "addon_updateChannel": None,
+    # Add-on license such as GPL 2
+    "addon_license": "GPL 2",
+    # URL for the license document the ad-on is licensed under
+    "addon_licenseURL": "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html",
 }
 
 # Define the python files that are the sources of your add-on.
@@ -69,7 +68,7 @@ excludedFiles = []
 # Base language for the NVDA add-on
 # If your add-on is written in a language other than english, modify this variable.
 # For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
-baseLanguage = "en"
+baseLanguage = "fr"
 
 # Markdown extensions for add-on documentation
 # Most add-ons do not require additional Markdown extensions.
